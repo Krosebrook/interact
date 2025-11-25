fc58c26384e2d4a579187f02bf2d7a4aa497d8ad
Kyle <kylerosebrook@gmail.com>
2025-11-25 11:01:59 +0000

    File changes

--- src/components/hooks/useEventData.jsx
+++ src/components/hooks/useEventData.jsx
@@ -1,41 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
 
-const STALE_TIME = 2 * 60 * 1000; // 2 minutes
-
-export function useEventData(options = {}) {
-  const { limit = 100, enableParticipations = true } = options;
-
-  const { data: events = [], isLoading: eventsLoading, refetch: refetchEvents } = useQuery({
+export function useEventData() {
+  const { data: events = [], isLoading: eventsLoading } = useQuery({
     queryKey: ['events'],
-    queryFn: () => base44.entities.Event.list('-scheduled_date', limit),
-    staleTime: STALE_TIME
+    queryFn: () => base44.entities.Event.list('-scheduled_date', 100)
   });
 
-  const { data: activities = [], isLoading: activitiesLoading, refetch: refetchActivities } = useQuery({
+  const { data: activities = [], isLoading: activitiesLoading } = useQuery({
     queryKey: ['activities'],
-    queryFn: () => base44.entities.Activity.list(),
-    staleTime: STALE_TIME
+    queryFn: () => base44.entities.Activity.list()
   });
 
-  const { data: participations = [], isLoading: participationsLoading, refetch: refetchParticipations } = useQuery({
+  const { data: participations = [], isLoading: participationsLoading } = useQuery({
     queryKey: ['participations'],
-    queryFn: () => base44.entities.Participation.list(),
-    enabled: enableParticipations,
-    staleTime: STALE_TIME
+    queryFn: () => base44.entities.Participation.list()
   });
-
-  const refetchAll = () => {
-    refetchEvents();
-    refetchActivities();
-    if (enableParticipations) refetchParticipations();
-  };
 
   return {
     events,
     activities,
     participations,
-    isLoading: eventsLoading || activitiesLoading || (enableParticipations && participationsLoading),
-    refetchAll
+    isLoading: eventsLoading || activitiesLoading || participationsLoading
   };
 }
